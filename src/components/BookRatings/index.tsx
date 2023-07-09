0b6508ab665545767583ad975d36fbe37edb4e9c
Marina Portugal <marinapsvreis@gmail.com>
2023-07-08 22:24:58 -0300

    :sparkles: Doesnt show Avaliar button if the user already rated the book

--- src/components/BookRatings/index.tsx
+++ src/components/BookRatings/index.tsx
@@ -1,38 +1,53 @@
-import { useState } from "react"
-import { RatingStars } from "../RatingStars"
-import { Text } from "../Typography"
-import { RatingWithAuthor, UserRatingCard } from "../UserRatingCard"
-import { Link } from "../ui/Link"
-import { Container } from "./styles"
+import { useSession } from "next-auth/react"
+import { Fragment, useState } from "react"
+import { LoginDialog } from "../LoginDialog"
 import { RatingForm } from "../RatingForm"
+import { Text } from "../Typography"
+import { Link } from "../ui/Link"
+import { RatingWithAuthor, UserRatingCard } from "../UserRatingCard"
+import { Container } from "./styles"
 
 type BookRatingsProps = {
   ratings: RatingWithAuthor[]
   bookId: string
 }
 
-export const BookRatings = ({bookId, ratings}: BookRatingsProps) => {
+export const BookRatings = ({ bookId, ratings }: BookRatingsProps) => {
+  const { status, data: session } = useSession();
   const [showForm, setShowForm] = useState(false)
 
+  const isAuthenticated = status === "authenticated"
+
   const handleRate = () => {
+    if(!isAuthenticated) return;
     setShowForm(true)
   }
 
-  const sortedRatingsByDate = ratings.sort((a,b) => {
+  const RatingWrapper = isAuthenticated ? Fragment : LoginDialog
+
+  const sortedRatingsByDate = ratings.sort((a, b) => {
     return new Date(b.created_at).getTime() - new Date(a.created_at).getTime()
   })
+
+  const canRate = ratings.every(x => x.user_id !== session?.user?.id);
+
+  console.log(session)
 
   return (
     <Container>
       <header>
         <Text>Avaliações</Text>
-        <Link withoutIcon onClick={handleRate} text="Avaliar" />
+        {canRate && (
+          <RatingWrapper>
+            <Link withoutIcon onClick={handleRate} text="Avaliar" color="purple" />
+          </RatingWrapper>
+        )}
       </header>
 
       <section>
         {showForm && <RatingForm bookId={bookId} onCancel={() => setShowForm(false)} />}
-        {sortedRatingsByDate.map((rating) => (
-          <UserRatingCard key={rating.id} rating={rating}/>
+        {sortedRatingsByDate.map(rating => (
+          <UserRatingCard key={rating.id} rating={rating} />
         ))}
       </section>
     </Container>
